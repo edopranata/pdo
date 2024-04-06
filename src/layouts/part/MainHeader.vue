6b0d165db6d4a5dd5a20d2a4224261a9d9135e81
edopranata <edo.pranata@gmail.com>
2024-04-07 00:44:28 +0700

    Change Main Header

--- src/layouts/part/MainHeader.vue
+++ src/layouts/part/MainHeader.vue
@@ -7,8 +7,8 @@ const {toggleLeftDrawer, toggleRightDrawer} = usePageStore()
 <template>
   <q-toolbar class="glossy">
     <q-btn flat round icon="menu" @click="toggleLeftDrawer" />
-    <q-btn outline round label="mms" to="/admin" class="tw-ml-2" />
-    <q-toolbar-title>MAY MAY</q-toolbar-title>
+    <q-btn outline round label="PDO" to="/admin" class="tw-ml-2" />
+    <q-toolbar-title>Palm Delivery Order</q-toolbar-title>
 
     <q-btn dense flat icon="manage_accounts" round @click="toggleRightDrawer"/>
   </q-toolbar>
